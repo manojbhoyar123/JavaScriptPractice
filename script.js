f4241f7a2141daa7bbabed7def2091ac84b7edb6
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-10 00:52:21 +0530

    today's task

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("from external script:\n");
+var firstName,age;
+var schname,isSmart;
+firstName = "Sarvadnya";
+age = 5;
+schName = "Kennedy";
+isSmart = true;
+console.log(firstName + " " + "AND " + " " + age + " " + schName + " "+ isSmart);
