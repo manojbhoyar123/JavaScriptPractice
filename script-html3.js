65894edeb92fa2dc580782b12518d0fd86de1573
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-13 00:45:31 +0530

    13thOCT

---
+++ script-html3.js
@@ -0,0 +1,14 @@
+var johnMass,johnHeight,markMass,markHeight;
+johnMass=prompt("Enter john's mass:-");
+johnHeight = prompt("Enter john's height:-");
+markMass = prompt("Enter Mark's mass:-");
+markHeight = prompt("Enter Mark's Height:-");
+var bmiJohn,bmiMark,isMarksBmiHigher;
+bmiJohn = (johnMass/(johnHeight*johnHeight));
+bmiMark = (markMass/(markHeight*markHeight));
+
+isMarksBmiHigher = bmiMark > bmiJohn;
+console.log("is mark's BMI higher than john",isMarksBmiHigher);
+
+
+
