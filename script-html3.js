7a74e09a2a1feba223e23210e20b735bde42170a
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-13 21:40:58 +0530

    13thoct

--- script-html3.js
+++ script-html3.js
@@ -1,16 +1,62 @@
 var johnMass,johnHeight,markMass,markHeight;
-johnMass=prompt("Enter john's mass:-");
-johnHeight = prompt("Enter john's height:-");
-markMass = prompt("Enter Mark's mass:-");
-markHeight = prompt("Enter Mark's Height:-");
-var bmiJohn,bmiMark,isMarksBmiHigher;
+var bmiJohn, bmiMark, isMarksBmiHigher;
+// johnMass=prompt("Enter john's mass:-");
+//johnHeight = prompt("Enter john's height:-");
+//markMass = prompt("Enter Mark's mass:-");
+//markHeight = prompt("Enter Mark's Height:-");
+johnMass = 55;
+johnHeight = 1.5;
+markMass = 66;
+markHeight = 1.9;
 bmiJohn = (johnMass/(johnHeight*johnHeight));
 bmiMark = (markMass/(markHeight*markHeight));
 
 isMarksBmiHigher = bmiMark > bmiJohn;
 console.log(bmiJohn,bmiMark)
 console.log("is mark's BMI higher than john:- " + isMarksBmiHigher);
-alert("is mark's BMI higher than john:- ", isMarksBmiHigher);
-
+//alert("is mark's BMI higher than john:- ", isMarksBmiHigher);
+var firstName = "John";
+var status = "single";
+var age = 21;
+if (status === "married")
+{
+    console.log(firstName + " is " + status);
+} 
+else
+{
+    console.log(firstName + " is not " + status);
+}
+var isMarried = true;
+if(isMarried)
+{
+    console.log(firstName + " is married ");
+}
+else
+{
+    console.log(firstName + " is not married ");
+}
+if(bmiMark > bmiJohn)
+{
+    console.log("Mark's BMI is higher than john");
+}
+else
+{
+    console.log("John's BMI is higher than Mark");
+}
+var age1 , drink ;
+age1 = 11;
+age1 >= 18 ? console.log(firstName + " can drink bear:- "): console.log(firstName + " can drink juice: -")
+drink = (age1 >= 18 ? "beer" : "Juice");
+console.log(drink);
+job = "teacher";
+switch(job)
+{
+    case "teacher":
+        console.log(firstName + " likes teaching ");
+}
+    case "coder";
+    {
+        console.log(firstName + " likes coding " );
+    }
 
 
