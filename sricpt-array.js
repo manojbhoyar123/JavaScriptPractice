674608f9c37f018ef0f38a5f759cbfbe4b9a2453
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-20 20:22:52 +0530

    20th

--- sricpt-array.js
+++ sricpt-array.js
@@ -1,2 +1,9 @@
 var name = ["abc","qwe","w345"];
 var year = [1983,1984,1985];
+var smplus = ("names are when + added:-" + name);
+var smcomma = ("names" ,name);
+console.log(smplus);
+console.log(smcomma);
+console.log(typeof(smplus));
+console.log(typeof(smcomma));
+
