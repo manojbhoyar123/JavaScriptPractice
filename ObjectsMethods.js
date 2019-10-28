642e42badff07dd890dbd829589502f05a99aa92
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-29 00:18:27 +0530

    29thOCT

--- ObjectsMethods.js
+++ ObjectsMethods.js
@@ -1,18 +1,16 @@
 var manojObj = {
     name: 'Manoj',
     prof: 'swCoder',
-    techn: 'javaScript',
     birthyear: 1983,
     family:['Baba','Aai','Dipali','Sachin','Rohini','Sarvadnya','Sarvesh'],
-    ismarried: true,
     calcAge: function(birthyear){
         //return 2019 - this.birthyear;   //this helps to assign local value
         this.age = 2019 - this.birthyear;
     }
 };
+// addition and modification of object property from outside object definition
+manojObj.techn = 'javaScript';
+manojObj['isMarried'] = true;  //String can be defined like this, i.e without . in it
 //console.log(manojObj);
-console.log(manojObj.calcAge(1986));   // since, we are using value from object, this becomes undefined.
 
-manojObj.calcAge();
-console.log(manojObj);
 
