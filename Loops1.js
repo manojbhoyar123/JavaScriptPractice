6120ae2d94f2d97e64445f18c8f228e7177f0615
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-29 23:54:35 +0530

    11

---
+++ Loops1.js
@@ -0,0 +1,21 @@
+var i=0;
+for (i; i <= 20; i+=5)
+{
+    console.log(i);
+}
+var johnA = ['john','smith','alfred','williams'];
+for (var j=0; j < johnA.length; j++)
+{
+    console.log(johnA[j]);
+}
+//j = 0;
+while (j < johnA.length)
+{
+    console.log(johnA[j]);
+}
+
+
+
+
+
+
