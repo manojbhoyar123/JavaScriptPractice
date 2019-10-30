6790fca1f04dfe87ff52771cef7e2f0331e6b0b3
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-10-31 00:10:42 +0530

    d

--- Loops1.js
+++ Loops1.js
@@ -4,35 +4,27 @@ var k = 0;
 var l = 0;
 var m = 0;
 
-for (i; i <= 20; i+=5)
-{
+for (i; i <= 20; i+=5){
     console.log(i);
 }
-
 var johnA = ['john', 'smith', 'alfred', 3, 'williams'];
-for (j; j < johnA.length; j++)
-{
+for (j; j < johnA.length; j++){
     console.log(johnA[j]);
 }
-
-while (k < johnA.length)
-{
+while (k < johnA.length){
     console.log(johnA[k]);
     k = k+1;
 } 
-
-for (l; l< johnA.length; l++)
-{
+for (l; l< johnA.length; l++){
     if(typeof(johnA[l]) !== 'string') continue;  // !== is equal to ===
     {
         console.log(johnA[l]);
     }
 }
-for (m; k< johnA.length ; m++)
-{
+for (m; m< johnA.length ; m++){
     if(typeof (johnA[m]) !== 'string') break;
     {
-        
+        console.log(johnA[m]);
     }
 }
 
