909bba86ae4cc804ea7b6cd9694828fe22fa0805
Manojkumar <manojbhoyar123@gmail.com>
2019-11-05 00:44:28 +0530

    Update CodingChallenge.js

--- CodingChallenge.js
+++ CodingChallenge.js
@@ -25,3 +25,26 @@ var john = {
 john.calcTips();
 console.log(john);
 
+var MarkCL = {
+    Fname: 'MarkCL',
+    bills: [124, 48, 264, 180, 42],
+    calcTips: function () {
+        this.tips = [];
+        this.finalValues = [];
+        // tip calculation
+        for (var i = 0; i < this.bills.length; i++) {
+            var prcnt;
+            var bill = this.bills[i];
+            if (bill < 50) { prcnt = 0.2; }
+            else if (bill >= 50 && bill <= 200) { prcnt = 0.15; }
+            else { prcnt = 0.1; }
+            // adding result in array
+            this.tips[i] = prcnt * bill;
+            this.finalValues[i] = bill + bill * prcnt;
+        }
+    }
+};
+//console.log(john.calcTips()); this will give undefined
+MarkCL.calcTips();
+console.log(MarkCL);
+
