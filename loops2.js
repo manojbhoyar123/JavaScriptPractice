44c5384fc49dd6e8f2ab5949b275f73ee73fde54
Manojkumar <41335338+manojbhoyar123@users.noreply.github.com>
2019-11-07 08:35:51 +0530

    d
     d

---
+++ loops2.js
@@ -0,0 +1,25 @@
+var johnLoop2 = ['john', 'smith', 'alfred', 3, 'williams'];
+var l2;
+for(l2 = (johnLoop2.length -1); l2 >= 0; l2--)
+{
+    console.log(johnLoop2[l2]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
