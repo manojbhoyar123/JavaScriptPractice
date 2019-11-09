d650cc44d58bd6eb1a6ad4d85c295a4023219c27
Manojkumar <manojbhoyar123@gmail.com>
2019-11-09 23:45:22 +0530

    y

--- Scoping2.js
+++ Scoping2.js
@@ -1,16 +1,19 @@
-var john ={          //object starts with keyword var
-name:'John',
-yearOfBirth:1983,
-calcAge: function()      //function declaration must have enclosed brackets.
-    {
+var johnW ={          //object starts with keyword var
+name:'JohnW',
+yearOfBirth: 1990,
+calculateAge: function() {    //function declaration must have enclosed brackets.
         console.log(this);
-        //console.log(2019 - john.yearOfBirth);
         console.log(2019 - this.yearOfBirth);
-        function inerfunction()
-        {
-            console.log(this);  // return windows objects
         }
-        inerfunction();   //calling function
-    }
+}
+johnW.calculateAge();
+
+var mikeW = {
+    name: 'Mike',
+    yearOfBirth: 1989
 };
-john.calcAge();
+mikeW.calculateAge = johnW.calculateAge;
+mikeW.calculateAge();
+
+
+
