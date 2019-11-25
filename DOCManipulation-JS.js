b775e7ff89b187963a67fd64048d15c991798079
Manojkumar <manojbhoyar123@gmail.com>
2019-11-25 23:36:52 +0530

    25thNOV

--- DOCManipulation-JS.js
+++ DOCManipulation-JS.js
@@ -1,9 +1,10 @@
-var abc, xyz,a ,b, c,d;
-document.getElementById("name1").style.backgroundcolor = "Blue";
-abc = document.getElementById("name1").style.color = "black";
-console.log(abc);
+var abc, xyz,a ,b, c,d,e,f,g;
+b = document.getElementById("name1").style.backgroundcolor = "green";
+abc = document.getElementById("name1").style.color = "red";
+console.log(b);
+//console.log(abc);
 xyz = document.getElementsByClassName("btn-new");
-console.log(xyz);
+//console.log(xyz);
 /*
 <button class="btn-new"><i class="ion-ios-plus-outline"></i>New game</button>
     <button class="btn-roll"><i class="ion-ios-loop"></i>Roll dice</button>
@@ -11,10 +12,14 @@ console.log(xyz);
     < h1 > First header < /h1> 
     < h2 id = "Title" > this is the title for first header < /h2> */
 a = document.getElementsByTagName("p1");
+e = document.getElementsByTagName("p2");
+f = document.getElementsByClassName("elemnameFirst");
 console.log(a);
+console.log(e);
+console.log(f);
 c = document.querySelector("h1");  
 d = document.querySelector("#Title");
-console.log(c);
-console.log(d);
+//console.log(c);
+//console.log(d);
 
 
