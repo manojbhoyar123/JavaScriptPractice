5e7c0fdf8d2072ad4b7b5f1e85e57fbf22d4b9b2
Manojkumar <manojbhoyar123@gmail.com>
2019-12-08 23:39:07 +0530

    A

--- 4-DOM-pig-game/starter/app1.js
+++ 4-DOM-pig-game/starter/app1.js
@@ -1,10 +1,18 @@
-var score , roundscore , activeplayer , dice;
-score = [0,0];
+var scores , roundScore , activePlayer , dice , dice2;
+scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 function getDiceValue()
 {
-    dice = Math.floor(Math.random() * 6);
-    console.log(dice);
+    dice2 = Math.floor(Math.random() * 6) + 1;
+    console.log(dice2);
 }
-document.querySelector('#score-0').textContent = getDiceValue();
+dice = Math.floor(Math.random() * 6) + 1;
+//document.querySelector('#current-0').textContent = getDiceValue(); //always select first element in DOC. Not working as function
+//textContent always needs text.
+//document.querySelector('#current-0').textContent = dice;  //this is basic methodbundleRenderer.renderToStream
+document.querySelector('#current-' + activePlayer).textContent = dice; //JS convert this to current-0 or current-1
+
+
+
+
