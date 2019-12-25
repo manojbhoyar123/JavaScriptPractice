9dbc998d3c2d646b3f57c83aaad0d4de12eb44dd
Manojkumar <manojbhoyar123@gmail.com>
2019-12-25 09:55:16 +0530

    25th

--- 4-DOM-pig-game/starter/app1.js
+++ 4-DOM-pig-game/starter/app1.js
@@ -7,7 +7,7 @@ function getDiceValue()
     dice2 = Math.floor(Math.random() * 6) + 1;
     console.log(dice2);
 }
-dice = Math.floor(Math.random() * 6) + 1;
+dice = Math.floor(Math.random() * 6) + 1; //later this code was moved to function on line 27
 //document.querySelector('#current-0').textContent = getDiceValue(); //always select first element in DOC. Not working as function
 //textContent always needs text.
 //document.querySelector('#current-0').textContent = dice;  //this is basic methodbundleRenderer.renderToStream
@@ -20,11 +20,18 @@ document.querySelector('.dice').style.display = 'none';
 function btn(){
     console.log("buttonCalled");
 }
+btn();
 //document.querySelector('.btn-roll').addEventListener('click',btn()); //simplest way
 //declaring function separately is risky, since other module may call it,
 //below method is anonymous method to call function and is safer.
 document.querySelector('.btn-roll').addEventListener('click', function() {
     // 1. Random number
+    dice = Math.floor(Math.random() * 6) + 1;
+    document.querySelector('#current-' + activePlayer).textContent = dice;
+    if (dice !=1)
+    {
+        
+    }
     // 2. Display the result
     // 3. Update the round score only if random number is not equal to 1
     
