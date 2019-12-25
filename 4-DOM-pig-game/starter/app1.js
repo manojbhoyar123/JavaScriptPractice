4959f1771ea267be85432d5356026c4aab03d2ce
Manojkumar <manojbhoyar123@gmail.com>
2019-12-26 00:52:45 +0530

    26thdec

--- 4-DOM-pig-game/starter/app1.js
+++ 4-DOM-pig-game/starter/app1.js
@@ -2,11 +2,13 @@ var scores , roundScore , activePlayer , dice , dice2 , x;
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
+/*
 function getDiceValue()
 {
     dice2 = Math.floor(Math.random() * 6) + 1;
     console.log(dice2);
 }
+*/
 dice = Math.floor(Math.random() * 6) + 1; //later this code was moved to function on line 27
 //document.querySelector('#current-0').textContent = getDiceValue(); //always select first element in DOC. Not working as function
 //textContent always needs text.
