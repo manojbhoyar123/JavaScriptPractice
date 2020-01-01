95a41ccb0f404af15e5420addd44b499699e99b6
Manojkumar <manojbhoyar123@gmail.com>
2020-01-02 00:04:43 +0530

    2nd jan20

--- 4-DOM-pig-game/starter/app1.js
+++ 4-DOM-pig-game/starter/app1.js
@@ -36,14 +36,31 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice' + '-'+ dice + '.png';
     
     // 3. Update the round score only if random number is not equal to 1
-    if(dice !== 0)
+    if(dice !== 1)
     {
         roundScore = roundScore + dice;
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        scores.push(roundScore);
+        //const arrSum = arr => arr.reduce((a, b) => a + b, 0);
+        //document.querySelector('#score-' + activePlayer).textContent = scores;
     }
     else
     {
         //next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        // make current score to 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        // to play with active display panel
+        /*
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
+        */
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        document.querySelector('.dice').style.display = 'none';
+
     }
     console.log(diceDOM.src);
 
